2e30e39bbf7486c3d4e327e5a9df2cdd271d4e89
abluehope <100765576+abluehope@users.noreply.github.com>
2023-09-01 16:39:40 +0900

    Add files via upload

---
+++ js/casia.js
@@ -0,0 +1,33 @@
+$(function () {
+  $(".mopen").on("click", function () {
+    $(this).toggleClass("on");
+    $(".gnb").toggleClass("on");
+  });
+
+  $(".gnb").on("wheel touchmove", function (e) {
+    e.preventDefault();
+  });
+
+  $(window).on("scroll", function () {
+    let sct = $(window).scrollTop();
+    if (sct > 0) {
+      $(".header").addClass("on");
+    } else {
+      $(".header").removeClass("on");
+    }
+  });
+
+  $(".mainSlide").slick({
+    fade: true,
+    dots: true,
+    arrows: false,
+  });
+
+  $(".mainVisual .arrows .left").on("click", function () {
+    $(".mainSlide").slick("slickPrev");
+  });
+
+  $(".mainVisual .arrows .right").on("click", function () {
+    $(".mainSlide").slick("slickNext");
+  });
+});
